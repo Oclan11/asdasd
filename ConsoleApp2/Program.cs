63009ce54816ee29f4695d07de3ad6592c8aad23
Bori_4vkz <fgfhdf@gmail.com>
2023-05-05 14:48:43 +0300

    1584

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] a = new int[10];
+        Random r = new Random();
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            a[i] = r.Next(-10, 10);
+            Console.Write(a[i] + ", ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.Write("Введите свое число: ");
+
+        int num = int.Parse(Console.ReadLine());
+        Ifs(a, num);
+    }
+
+    static void Ifs(int[] a, int b)
+    {
+        int c = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] < b)
+            {
+                c++;
+            }
+        }
+
+        Console.WriteLine(c);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# adolf
+# adolf
